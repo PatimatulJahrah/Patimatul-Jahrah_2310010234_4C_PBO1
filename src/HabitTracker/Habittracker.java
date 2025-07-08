9d285d123f76d9731d65601985558b20b1e51fc5
PatimatulJahrah <Fatzah052005@gmail.com>
2025-07-08 14:41:20 +0800

    Buat struktur dasar class HabitTracker dan import library

--- src/HabitTracker/Habittracker.java
+++ src/HabitTracker/Habittracker.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HabitTracker;
 
-/**
- *
- * @author fatzah
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Habittracker {
-    
+    public static void main(String[] args) {
+        
+    }
 }
