bab989663c653b11ae4c6a1c2f9e26203a4f012c
PatimatulJahrah <Fatzah052005@gmail.com>
2025-07-08 14:56:50 +0800

    Menambahkan atribut nama dan deskripsi

--- src/HabitTracker/Kebiasaan.java
+++ src/HabitTracker/Kebiasaan.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HabitTracker;
 
-/**
- *
- * @author fatzah
- */
 public class Kebiasaan {
+    private String nama;
+    private String deskripsi;
+
     
 }
