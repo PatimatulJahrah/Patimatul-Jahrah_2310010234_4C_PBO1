a71ddbcb326998c44a652673c261d81ed5b14bb6
PatimatulJahrah <Fatzah052005@gmail.com>
2025-07-08 14:59:10 +0800

    Menambahkan constructor, getter dan setter

--- src/HabitTracker/Kebiasaan.java
+++ src/HabitTracker/Kebiasaan.java
@@ -4,5 +4,24 @@ public class Kebiasaan {
     private String nama;
     private String deskripsi;
 
-    
+    public Kebiasaan(String nama, String deskripsi) {
+    this.nama = nama;
+    this.deskripsi = deskripsi;
+}
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
 }
